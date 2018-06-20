3298fdd3edf9d4bda16495ff81187bfdacf836f9
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-06-20 15:07:59 +0200

    Configuration
    Ajout du fichier de configuration.

--- src/fr/Maxime3399/MCube/MainClass.java
+++ src/fr/Maxime3399/MCube/MainClass.java
@@ -1,7 +1,40 @@
 package fr.Maxime3399.MCube;
 
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainClass extends JavaPlugin{
+	
+	private static Plugin plugin;
+	
+	public void onEnable() {
+		
+		plugin = this;
+		
+		File f = new File(getDataFolder(), "config.yml");
+		if(!f.exists()) {
+			
+			getConfig().options().copyDefaults(true);
+			saveDefaultConfig();
+			
+			Bukkit.getConsoleSender().sendMessage("§6§l[§r§3M-Cube§6§l]§r §eLe fichier de configuration \"config.yml\" a été créé. Merci de configurer le plugin.");
+			disable();
+			
+		}else {
+			
+			//suite
+			
+		}
+		
+	}
+	
+	private static void disable() {
+		
+		Bukkit.getPluginManager().disablePlugin(plugin);
+		
+	}
 
 }
