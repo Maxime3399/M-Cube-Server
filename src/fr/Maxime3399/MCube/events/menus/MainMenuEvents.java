aadb89c8ed29876e266d2cbcd1f12fce5d422fdc
Maxime3399 <maxime3399.minecraft@gmail.com>
2018-06-21 18:43:14 +0200

    Main Menu Events Place
    Première mise en place du main menu events.

--- src/fr/Maxime3399/MCube/events/menus/MainMenuEvents.java
+++ src/fr/Maxime3399/MCube/events/menus/MainMenuEvents.java
@@ -1,7 +1,39 @@
 package fr.Maxime3399.MCube.events.menus;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class MainMenuEvents implements Listener {
+	
+	@EventHandler
+	public void onClick(InventoryClickEvent e) {
+		
+		if(e.getAction() == null) {
+			
+			return;
+			
+		}else if(e.getInventory().getName().equalsIgnoreCase("§8Menu Principal")) {
+			
+			if(e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {
+				
+				return;
+				
+			}else {
+				
+				String item = e.getCurrentItem().getItemMeta().getDisplayName();
+				Player p = (Player) e.getWhoClicked();
+				
+				//suite
+				
+			}
+			
+			e.setCancelled(true);
+			
+		}
+		
+	}
 
 }
